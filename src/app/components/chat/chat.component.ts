1d6d97f6fc6f63d16b2fdb8cf91022f0941e98ec
Agon Xhaferi <agonxhaferi99@gmail.com>
2024-07-31 23:26:53 +0200

    Added more code (not necessarily good code.) to the chat.component.ts

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -4,6 +4,9 @@ import {MatFormField, MatInput, MatLabel} from "@angular/material/input";
 import {NgForOf} from "@angular/common";
 import {ChatService} from "../../services/chat.service";
 import {ActivatedRoute} from "@angular/router";
+import {concatMap, from, zip} from "rxjs";
+import {MessageDto} from "../dto/message.dto";
+import Session from "supertokens-web-js/recipe/session";
 
 @Component({
   selector: 'app-chat',
@@ -21,15 +24,55 @@ export class ChatComponent implements OnInit, OnDestroy {
   socket?: Socket
   messages: string[] = [];
 
+  userId: string | undefined
+  selectedUserId: string | undefined//I'm thinking of just saving this on localStorage at this point. I'm making 100 calls.
+
+  mainUsersMessages: MessageDto[] = []
+  selectedUsersMessages: MessageDto[] = []
+
   constructor(private _chatService: ChatService,
               private _activatedRoute: ActivatedRoute,) {
   }
 
   ngOnInit(): void {
-    debugger;
-    this.socket = io('http://localhost:3001');
+    this._activatedRoute.paramMap.pipe(
+      concatMap(paramMap => {
+        const chatId = paramMap.get('chatId')
+        if (!chatId) {
+          alert('chatId param missing')
 
-    this.socket.on('message', (data: string) => this.messageListener(data))
+          throw new Error('chatId param must be provided')
+        }
+
+        return zip([
+            this._chatService.findChatById(chatId),
+            from(Session.getUserId())
+          ]
+        )
+      })
+    ).subscribe(([
+                   chat,
+                   mainUserId
+                 ]) => {
+      if (chat.userOneId === mainUserId) { //TODO: Theres 1000% a better way to do this.
+        this.userId = chat.userOneId
+        this.selectedUserId = chat.userTwoId
+      } else {
+        this.userId = chat.userTwoId
+        this.selectedUserId = chat.userOneId
+      }
+
+      chat.userMessages.forEach(userMessageDto => {
+        if (userMessageDto.senderId === mainUserId) {
+          this.mainUsersMessages.push(userMessageDto)
+        } else {
+          this.selectedUsersMessages.push(userMessageDto)
+        }
+      })
+
+      this.socket = io('http://localhost:3001');
+      this.socket.on('message', (data: string) => this.messageListener(data))
+    })
   }
 
   send(messageValue: string) {
--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,7 +1,9 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {addWarning} from "@angular-devkit/build-angular/src/utils/webpack-diagnostics";
 import {AuthService} from "../../services/auth.service";
 import {SpinnerService} from "../../services/spinner.service";
+import SuperTokens from "supertokens-web-js";
+import Session from "supertokens-web-js/recipe/session";
 
 @Component({
   selector: 'app-profile',
@@ -10,11 +12,18 @@ import {SpinnerService} from "../../services/spinner.service";
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
   //TODO: Add logic here regarding the current user data/ password change etc. etc..
+  userId: string | undefined
+
   constructor(private authService: AuthService,
               private spinnerService: SpinnerService) {
   }
+
+  async ngOnInit(): Promise<void> {
+    this.userId = await Session.getUserId()
+  }
+
 
   async signOut() {
     this.spinnerService.show()
